403270ff4461f5520890ebeea49eb960f72fd4e4
Tomomi Kanai <tomomin001104@gmail.com>
2020-11-03 01:07:29 +0900

    ねむい

--- demo/firebase_all_in_one/firebase_all_in_one.ino
+++ demo/firebase_all_in_one/firebase_all_in_one.ino
@@ -6,27 +6,39 @@
 #include "porthub.h"
 #define SERVO_ADDR 0x53
 
+//****初期値設定****
+int num = 0;//表情のパターン
+int angle = 0;//角度センサー
+int before_angle = 0;//角度センサーの前の数値と比較する用
+
+int count = 0;//loopの周期をカウント
+
+int now_received_angle=0;//相手から受け取った角度
+int received_angle=0;//以前受け取った値と比較する用
+
+int now_received_face=0;//相手から受け取った表情
+int received_face=0;//以前受け取った値と比較する用
+
+int changed = 0;
+int fluency = 10;
+
 //PbHUB UNIT
 PortHub porthub;
 uint8_t HUB_ADDR[6]={HUB1_ADDR,HUB2_ADDR,HUB3_ADDR,HUB4_ADDR,HUB5_ADDR,HUB6_ADDR};
 
-//初期値設定
-int num = 0;
-int angle = 0;
-int before_angle = 0;
-
 //wifi
-#define WIFI_SSID "***********"//自分のルーターのSSIDに変更してください
-#define WIFI_PASSWORD "***********" //自分のルーターのパスワードに変更してください
+#define WIFI_SSID "************"//自分のルーターのSSIDに変更してください
+#define WIFI_PASSWORD "****************" //自分のルーターのパスワードに変更してください
 
 // FirebaseのデータベースURL（自分のデータベースURLに変更してください）
-#define FIREBASE_DATABASE_URL "m5data-f5280.firebaseio.com"
+#define FIREBASE_DATABASE_URL "m5data2-868b2.firebaseio.com"
 
 //*****セットアップ******************
 void setup() {
-  M5.begin(true, false, true);
+  M5.begin();
   M5.Power.begin();
-
+  Serial.begin(115200);
+  
   // Wi-Fi接続
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
@@ -34,7 +46,6 @@ void setup() {
     Serial.print(".");//wifiに繋がらないと永遠と....が出てくる
     delay(500);
   }
-  Serial.println();
 
   // Firebase初期化
    Firebase.begin(FIREBASE_DATABASE_URL);
@@ -42,8 +53,6 @@ void setup() {
   // WiFi Connected
   Serial.println("\nWiFi Connected.");
   Serial.println(WiFi.localIP());
-  M5.Lcd.setTextSize(5);
-  M5.Lcd.setCursor(10, 100);
 
   //サーボモーター
   Wire.begin(21, 22, 100000);
@@ -51,11 +60,18 @@ void setup() {
   //PbHUB UNIT
   porthub.begin();
 
+  eye(num%6);
 
 }
 
 
 //****サーボモーター回転********************
+//void Servo_write_angle(uint8_t number, uint8_t angle) {
+//    Wire.beginTransmission(SERVO_ADDR);
+//    Wire.write(0x10 | number);
+//    Wire.write(angle);
+//    Wire.endTransmission();
+//}
 void Servo_write_us(uint8_t number, uint16_t us) {
     Wire.beginTransmission(SERVO_ADDR);
     Wire.write(0x00 | number);
@@ -63,63 +79,91 @@ void Servo_write_us(uint8_t number, uint16_t us) {
     Wire.write(us >> 8 & 0x00ff);
     Wire.endTransmission();
 }
-
-
 //****メインループ********************
+void send(int angle){
+  Firebase.setInt("/M5Stack/dog/angle", angle);
+  return;
+}
+
 void loop() {
   M5.update();
-  M5.Lcd.setCursor(10, 100);
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.print(num);
-
+  
   //***実行内容***
-  //A,Cボタンで選択したメッセージ（アバター）をBボタンで送る
-  //回転角センサーの値を常時送る
+  //A,Cボタンで選択したアバターをBボタンで送る
+  //回転角センサーの値が変化した場合、送る
   //相手がメッセージ或いは回転角センサーの値を更新したら、それを検知して変更を反映させる
     
   //Aボタン→減少、Cボタン→増加　Bボタン→データ送信
   if (M5.BtnA.wasReleased()) {
     num--;
+    M5.Lcd.fillScreen(BLACK);
+    eye(num%6);
   }
 
   if (M5.BtnC.wasPressed()){
     num++;
+    M5.Lcd.fillScreen(BLACK);
+    eye(num%6);
   }
   
   if(M5.BtnB.wasReleased()) {
-    Firebase.setInt("/M5Stack/message/num", num);
-    Firebase.setInt("/M5Stack/message/new", 2222);
-    M5.Lcd.setCursor(10, 100);
+    M5.Lcd.setCursor(100, 200);
+    M5.Lcd.setTextSize(5);
     M5.Lcd.print("send!");
-    delay(1000);
-
+    Firebase.setInt("/M5Stack/dog/face", num);
+    M5.Lcd.fillScreen(BLACK);
+    eye(num%6);
   }
 
-  //angleが前の値よりも10以上変化したら数値を送る
+  //角度センサーの読み取り
   angle = porthub.hub_a_read_value(HUB_ADDR[0]);
-  if(abs(before_angle-angle)>10){
-    Firebase.setInt("/M5Stack/angle/num", angle);
-    Firebase.setInt("/M5Stack/angle/new", 2222);
-    Serial.print(angle);
-    before_angle = angle;
+  Serial.print(angle);
+  Serial.println("\n");
+
+  //前と比べて50以上変化あれば送信(負荷を減らすため)
+  if(abs(before_angle-angle)>50&&fluency!=2){
+    send(angle);
   }
-//
-//  //new=1111になっているのは相手が更新した証拠
-//  //値を受け取り、サーボモーター(0番)を回転させ、new=0に戻す
-//  if(int(Firebase.getFloat("/M5Stack/angle/new"))==2222){
-//    int received_angle = Firebase.getFloat("/M5Stack/angle/num");
-//    //0-760 →　-90～90にする
-//    received_angle -= 380;
-//    int moving_angle = received_angle * 9/38;
-//    Firebase.setInt("/M5Stack/angle/check", moving_angle);
-//    Servo_write_us(0,moving_angle);
-//    Firebase.setInt("/M5Stack/angle/new", 0);
+  before_angle = angle;
+
+//  if(abs(changed-count)<5){
+//    fluency = 2;
+//  }else{
+//    fluency = 10;
 //  }
-//  
-//  //上同様にメッセージを受け取る
-//  if(int(Firebase.getFloat("/M5Stack/message/new"))==1111){
-//    num =  int(Firebase.getFloat("/M5Stack/message/num"));
-//    Firebase.setInt("/M5Stack/num/new", 0);
-//  }
+  //10回に１回or毎回データベースにアクセスし、変化があるか確認する
+  if(count%3==0){
+    now_received_angle = int(Firebase.getFloat("/M5Stack/cat/angle"));
+    
+    if(now_received_angle!=received_angle){
+      Servo_write_us(0,now_received_angle*3);
+      
+      Serial.print("received");
+      Serial.print(now_received_angle);
+      Serial.println("\n");
+      received_angle = now_received_angle;
+      changed = count;
+    }
+  }
+  //10回に１回データベースにアクセスし、変化があるか確認する
+  if(count%17==0){
+    now_received_face = int(Firebase.getFloat("/M5Stack/cat/face"));
+    if(now_received_face!=received_face){
+      num = now_received_face;   
+      Serial.print("received");
+      Serial.print(num);
+      Serial.println("\n");
+      received_face = now_received_face;
+      
+      M5.Lcd.fillScreen(BLACK);
+      eye(num%6);
+      M5.Lcd.setCursor(100, 200);
+      M5.Lcd.setTextSize(5);
+      M5.Lcd.print("received!");
+    }
+  }
+  count++;
+
+  delay(100);
 
 }
